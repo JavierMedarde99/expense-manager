395993e7a12b8c6751bdc46bce67208a7175ab1b
JavierMedardeMata <32664879+JavierMedarde99@users.noreply.github.com>
2024-11-17 18:54:53 +0100

    done main page

--- web/src/main/resources/static/js/graph.js
+++ web/src/main/resources/static/js/graph.js
@@ -1,19 +1,97 @@
 
-const subType = ["videogames", "mtg", "book", "subsctiption", "peripherals", "clothes", "other"];
-
-var months = document.getElementsByClassName("month");
-var graph = document.getElementById("graph");
+let months = document.getElementsByClassName("month");
+let graph = document.getElementById("graph");
 if (months != null) {
-    months.forEach(month => {
-        var element = document.createElement("canvas");
-        document.body.insertBefore(element, graph);
-        var bills_id = document.getElementsByClassName(month+"_id");
-        bills_id.forEach(id =>{
-            var price = document.getElementById(bills_id+"_price");
-        })
-    });
+    for (let month of months) {
+        let subType = {
+            videogames: 0,
+            mtg: 0,
+            book: 0,
+            subsctiption: 0,
+            peripherals: 0,
+            clothes: 0,
+            other: 0,
+        }
+        let element = document.createElement("canvas");
+        let div = document.createElement("div");
+        let titel = document.createElement("h2");
+        titel.innerHTML = month.innerHTML;
+        titel.style.color = "white";
+        titel.style.textAlign = "center";
+        div.style.marginLeft =10;
+        graph.appendChild(div);
+        div.appendChild(titel)
+        div.appendChild(element);
+        let bills_id = document.getElementsByClassName(month.innerHTML + "_id");
+        for (let id of bills_id) {
+            let price = document.getElementById(month.innerHTML + "_" + id.innerHTML + "_price");
+            let amount = document.getElementById(month.innerHTML + "_" + id.innerHTML + "_amount");
+            let type = document.getElementById(month.innerHTML + "_" + id.innerHTML + "_type");
+            console.log(price.innerHTML);
+            let result_bill = price.innerHTML * amount.innerHTML;
+            switch (type.innerHTML) {
+                case "videogames":
+                    subType.videogames = subType.videogames + result_bill;
+                    break;
+                case "mtg":
+                    subType.mtg = subType.mtg + result_bill;
+                    break;
+                case "book":
+                    subType.book = subType.book + result_bill;
+                    break;
+                case "subsctiption":
+                    subType.subsctiption = subType.subsctiption + result_bill;
+                    break;
+                case "peripherals":
+                    subType.peripherals = subType.peripherals + result_bill;
+                    break;
+                case "clothes":
+                    subType.clothes = subType.clothes + result_bill;
+                    break;
+                default:
+                    subType.other = subType.other + result_bill;
+                    break;
+            }
+        }
+        let total = document.getElementById(month.innerHTML+"_total");
+        total = parseInt(total.innerHTML);
+        subType.videogames = (subType.videogames / total )*100;
+        subType.mtg = (subType.mtg / total )*100;
+        subType.book = (subType.book / total )*100;
+        subType.subsctiption = (subType.subsctiption / total )*100;
+        subType.peripherals = (subType.peripherals / total )*100;
+        subType.clothes = (subType.clothes / total )*100;
+        subType.other = (subType.other / total )*100;
+        generateGapth(Object.keys(subType),Object.values(subType),month.innerHTML,element);
+    }
+
 }
 
-function generateGapth(){
 
+function generateGapth(arrayKeys,arrayValues,month,element) {
+    console.log(arrayValues);
+        // Crea un nuevo objeto Chart con opciones
+        var chart = new Chart(element, {
+            // Configuración del tipo de gráfico
+            type: 'doughnut',
+    
+            // Datos del gráfico
+            data: {
+                labels: arrayKeys,
+                datasets: [{
+                    label: month,
+                    data: arrayValues,
+                    backgroundColor: [
+                        'rgb(137, 0, 79)',
+                        'rgb(127, 0, 0)',
+                        'rgb(133, 52, 0)',
+                        'rgb(135, 146, 0)',
+                        'rgb(0, 52, 0)',
+                        'rgb(0, 55, 133)',
+                        'rgb(27, 0, 75)'
+                    ],
+                    hoverOffset: 7
+                }]
+            }
+        });
 }
