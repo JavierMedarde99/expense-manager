d143ced045d8b4f0097887763e80c331642f957b
JavierMedardeMata <32664879+JavierMedarde99@users.noreply.github.com>
2025-03-28 23:07:52 +0100

    add footer

--- web/src/main/java/com/bills/web/entities/Users.java
+++ web/src/main/java/com/bills/web/entities/Users.java
@@ -4,6 +4,7 @@ import java.util.Collection;
 import java.util.Set;
 
 import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
@@ -11,6 +12,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.UniqueConstraint;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -28,6 +30,7 @@ public class Users implements UserDetails{
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(unique = true, nullable = false)
     private String userName;
     private String email;
     private String password;
